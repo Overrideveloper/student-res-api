4270c5c3552dd775a6afa2750fa1eef211b1a20c
overrideveloper <bansowisdom@gmail.com>
2017-10-06 21:55:54 +0100

    Added script for creating server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 //houses protocols to create server
+
+var express = require('express'),
+    app = express(),
+    port = process.env.port || 4000;
+
+app.listen(port);
+
+console.log(`School Resource Management API server started on ` + port);
