097b4e2eaa6b8d3c5b2bc35a3f22d1a5d29e84a2
overrideveloper <bansowisdom@gmail.com>
2017-10-07 16:51:48 +0100

    Modified server methods

--- server.js
+++ server.js
@@ -2,7 +2,32 @@
 
 var express = require('express'),
     app = express(),
-    port = process.env.port || 4000;
+    //start server on port 4000
+    port = process.env.port || 4000,
+    //mongoose instance
+    mongoose = require('mongoose'),
+    //load created data model
+    Student = require('./api/models/resModel'),
+    //body-parser instance
+    bodyparser = require('body-parser');
+
+//connect to MongoDB server
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/SchoolInfoDB');
+
+//use body-parser
+app.use(bodyparser.urlencoded({ extended : true }));
+app.use(bodyparser.json());
+
+//add 404 error code
+app.use(function(req, res){
+    res.status(404).send({ url: req.originalUrl + ' not found!'});
+});
+
+//import routes
+var routes = require('./api/routes/resRoutes');
+//register routes
+routes(app);
 
 app.listen(port);
 
