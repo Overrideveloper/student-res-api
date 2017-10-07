8c131be11a200475a984bfa011f4701c69fbe2bf
overrideveloper <bansowisdom@gmail.com>
2017-10-07 16:04:04 +0100

    Added routes

---
+++ api/routes/resRoutes.js
@@ -0,0 +1,21 @@
+/* Routing refers to determining how an application responds to a client request for a specific endpoint,
+which is a URI (or path) and a specific HTTP request method (GET, POST, and so on).
+Each of our routes has different route handler functions, which are executed when the route is matched.
+Below we have defined two basic routes(‘/students’, and ‘/students/:studentId’) with different methods
+‘/students has to methods(‘GET’ and ‘POST’), while ‘/students/:studentId’ has GET, PUT and DELETE.
+As you can see, we required the controller so each of the routes methods can call it’s respective handler function */
+
+'use strict';
+module.exports = function(app) {
+    var studentRes = require('../controllers/studentResController');
+
+    //Routes
+    app.route('/students')
+        .get(studentRes.list_students)
+        .post(studentRes.create(studentRes.create_students);
+
+    app.route('/students/:studentId')
+        .get(studentRes.read_student)
+        .put(studentRes.update_student)
+        .delete(studentRes.delete_student);
+};
