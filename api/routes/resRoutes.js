967600eb6a600ea3c5d4ec60eda9944073e46155
overrideveloper <bansowisdom@gmail.com>
2017-10-21 07:53:21 +0100

    Handlded Pre-flight Request

--- api/routes/resRoutes.js
+++ api/routes/resRoutes.js
@@ -12,6 +12,10 @@ module.exports = function(app) {
     app.use(function(req, res, next){
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        if (req.method === "OPTIONS") {
+            return res.status(200).end();
+        }
         next();
     });
 
