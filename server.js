7a67813a0521285a139d6b92ee82bc577e0f3e8f
overrideveloper <bansowisdom@gmail.com>
2017-10-23 22:43:05 +0100

    modified CORS logic

--- server.js
+++ server.js
@@ -41,13 +41,22 @@ app.use(function(req, res){
     res.status(404).send({ url: req.originalUrl + ' not found!'});
 });
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
+app.use(function (req, res, next) {
+    
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
 
